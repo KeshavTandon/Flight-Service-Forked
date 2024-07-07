8acb6d70bce8c9ed4aa85a45d189f21dc2962533
KeshavTandon <keshavtandon51@gmail.com>
2024-07-07 20:28:30 +0530

    Added CRUD for city model

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -21,6 +21,74 @@ async function createCity(data) {
     }
 }
 
+async function getCities() {
+  try {
+    const cities = await cityRepository.getAll();
+    return cities;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch data of all cities",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getCity(id) {
+  try {
+    const city = await cityRepository.get(id);
+    return city;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError("City you requested is not present", error.statusCode);
+    }
+    throw new AppError(
+      "Cannot fetch data of city",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function destroyCity(id) {
+  try {
+    const city = await cityRepository.destroy(id);
+    return city;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "City you requested to delete is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of city",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function updateCity(id, data) {
+  try {
+    const city = await cityRepository.update(id, data);
+    console.log(city);
+    return city;
+  } catch (error) {
+    if (error.statusCode == StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "city you requested to update is not present",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of city",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
-    createCity
+    createCity,
+    getCities,
+    getCity,
+    destroyCity,
+    updateCity
 }
