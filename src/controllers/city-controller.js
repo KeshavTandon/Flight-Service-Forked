82e56248ff2eb3bdc05175a270be9c7c6a91e68f
KeshavTandon <keshavtandon51@gmail.com>
2024-07-07 20:24:37 +0530

    Added CRUD for city model

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -24,7 +24,55 @@ async function createCity(req, res) {
                 .json(ErrorResponse);
     }
 }
+async function getCities(req, res) {
+  try {
+    const cities = await CityService.getCities();
+    SuccessResponse.data = cities;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function getCity(req, res) {
+  try {
+    const city = await CityService.getCity(req.params.id);
+    SuccessResponse.data = city;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function destroyCity(req, res) {
+  try {
+    const city = await CityService.destroyCity(req.params.id);
+    SuccessResponse.data = city;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function updateCity(req, res) {
+  try {
+    console.log(req.body.name);
+    const city = await CityService.updateCity(req.params.id, req.body);
+    SuccessResponse.data = city;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
 
 module.exports = {
-    createCity
+    createCity,
+    getCities,
+    getCity,
+    destroyCity,
+    updateCity
 }
--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -10,5 +10,12 @@ router.post('/',
         CityMiddlewares.validateCreateRequest,
         CityController.createCity);
 
+router.get("/", CityController.getCities);
+
+router.get("/:id", CityController.getCity);
+
+router.delete("/:id", CityController.destroyCity);
+
+router.patch("/:id", CityController.updateCity);
 
 module.exports = router;
